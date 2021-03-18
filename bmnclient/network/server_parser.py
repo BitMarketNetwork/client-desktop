e9a0e4a3283fac949ec16a163b3070cff5069cbc
dev <dev@bitmarket.network>
2021-03-18 22:27:35 +0300

    network.server_parser oprimization

--- bmnclient/network/server_parser.py
+++ bmnclient/network/server_parser.py
@@ -1,22 +1,56 @@
 # JOK++
 from __future__ import annotations
 
-from ..coins.coin import AbstractCoin
+from typing import Any, Optional, TYPE_CHECKING, Type
+
 from ..logger import Logger
+from ..utils.meta import classproperty
+
+if TYPE_CHECKING:
+    import logging
+    from ..coins.address import AbstractAddress
+    from ..coins.coin import AbstractCoin
+    from ..coins.tx import AbstractTx
 
 
-class ServerCoinParser:
+class ParseError(LookupError):
+    pass
+
+
+class AbstractServerParser:
+    __logger = None
+
+    @classproperty
+    def _logger(cls) -> logging.Logger:  # noqa
+        if not cls.__logger:
+            cls.__logger = Logger.getClassLogger(__name__, cls)  # noqa
+        return cls.__logger
+
     @classmethod
-    def parse(cls, server_response: dict, coin: AbstractCoin) -> bool:
+    def _parse(cls, item: dict, key_name: str, value_type: Type) -> Any:
         try:
-            offset = str(server_response["offset"])
-            unverified_offset = str(server_response["unverified_offset"])
-            unverified_hash = str(server_response["unverified_hash"])
-            height = int(server_response["height"])
-            verified_height = int(server_response["verified_height"])
-            status = int(server_response["status"])
-        except (KeyError, TypeError, ValueError) as e:
-            Logger.getClassLogger(__name__, cls).exception(e)
+            value = value_type(item[key_name])
+        except KeyError:
+            raise ParseError("Key \"{}\" not found.".format(key_name))
+        except (TypeError, ValueError):
+            raise ParseError("Invalid value for key \"{}\".".format(key_name))
+        return value
+
+
+class ServerCoinParser(AbstractServerParser):
+    @classmethod
+    def parse(cls, response: dict, coin: AbstractCoin) -> bool:
+        try:
+            offset = cls._parse(response, "offset", str)
+            unverified_offset = cls._parse(response, "unverified_offset", str)
+            unverified_hash = cls._parse(response, "unverified_hash", str)
+            height = cls._parse(response, "height", int)
+            verified_height = cls._parse(response, "verified_height", int)
+            status = cls._parse(response, "status", int)
+        except ParseError as e:
+            cls._logger.error(
+                "Failed to parse coin \"{}\": {}"
+                .format(coin.fullName, str(e)))
             return False
 
         # TODO legacy order
@@ -27,3 +61,76 @@ class ServerCoinParser:
         coin.verifiedHeight = verified_height
         coin.height = height
         return True
+
+
+class ServerTxParser(AbstractServerParser):
+    @classmethod
+    def parseList(cls, response: dict, address: AbstractAddress) -> bool:
+        if not isinstance(response, dict):
+            cls._logger.error(
+                "Invalid transaction list response for address \"{}\"."
+                .format(address.name))
+            return False
+
+        for (name, value) in response.items():
+            if not ServerTxParser.parse(name, value, address):
+                return False
+        return True
+
+    @classmethod
+    def parse(cls, name: str, response: dict, address: AbstractAddress) -> bool:
+        try:
+            # TODO validate name
+            height = cls._parse(response, "height", int)
+            time = cls._parse(response, "time", int)
+            amount = cls._parse(response, "amount", int)
+            fee = cls._parse(response, "fee", int)
+            coinbase = cls._parse(response, "coinbase", int) != 0
+
+            input_list = []
+            for item in response["input"]:
+                input_list.append(cls._parseIo(item, address))
+            output_list = []
+            for item in response["output"]:
+                output_list.append(cls._parseIo(item, address))
+        except ParseError as e:
+            cls._logger.error(
+                "Failed to parse transaction for address \"{}\": {}"
+                .format(address.name, str(e)))
+            return False
+
+        # TODO
+        from ..wallet.tx import Transaction
+        tx = Transaction(
+            address,
+            name,
+            amount,
+            fee,
+            coinbase,
+            input_list,
+            output_list)
+        tx.height = height
+        tx.time = time
+        address.appendTx(tx)
+        return True
+
+    @classmethod
+    def _parseIo(
+            cls,
+            item: dict,
+            address: AbstractAddress) -> Optional[AbstractTx.TxIo]:
+        try:
+            output_type = cls._parse(item, "output_type", str)
+            address_type = cls._parse(item, "type", str)
+            address_name = cls._parse(item, "address", str)
+            address_amount = cls._parse(item, "amount", int)
+        except ParseError as e:
+            raise ParseError(
+                "Failed to parse transaction IO: {}"
+                .format(str(e)))
+
+        # TODO
+        from ..wallet.address import CAddress
+        io = CAddress(address_name, address.coin)
+        io.amount = address_amount
+        return io
