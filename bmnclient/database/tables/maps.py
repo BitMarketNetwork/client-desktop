01d818137ac8e366a6ff2b86e80bf3de37ca5a08
dev <dev@bitmarket.network>
2022-04-06 00:05:27 +0300

    AddressTxMapTable:address_transaction_map -> AddressTransactionsTable:address_transactions

--- bmnclient/database/tables/maps.py
+++ bmnclient/database/tables/maps.py
@@ -1,30 +1,24 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from .table import (
     AbstractSerializableTable,
-    Column,
     ColumnEnum,
     ColumnValue,
-    RowListProxy)
+    ColumnValueType)
 from ...utils.class_property import classproperty
 
-if TYPE_CHECKING:
-    from typing import Any, Final, List, Tuple
 
-
-class AddressTxMapTable(
+class AddressTransactionsTable(
         AbstractSerializableTable,
-        name="address_transaction_map"):
+        name="address_transactions"):
     class ColumnEnum(ColumnEnum):
-        ROW_ID: Final = ()
+        ROW_ID = ()
 
-        ADDRESS_ROW_ID: Final = ("address_row_id", "INTEGER NOT NULL")
-        TX_ROW_ID: Final = ("tx_row_id", "INTEGER NOT NULL")
+        ADDRESS_ROW_ID = ("address_row_id", "INTEGER NOT NULL")
+        TX_ROW_ID = ("tx_row_id", "INTEGER NOT NULL")
 
     @classproperty
-    def _CONSTRAINT_LIST(cls) -> Tuple[str, ...]:  # noqa
+    def _CONSTRAINT_LIST(cls) -> tuple[str, ...]:  # noqa
         from .address import AddressListTable
         from .tx import TxListTable
         return (
@@ -44,33 +38,25 @@ class AddressTxMapTable(
         (ColumnEnum.ADDRESS_ROW_ID, ColumnEnum.TX_ROW_ID),
     )
 
-    def rowListProxy(self, *args, **kwargs) -> RowListProxy:
-        raise NotImplementedError
-
-    def insert(
-            self, cursor: Cursor,
-            address_row_id: int,
-            tx_row_id: int) -> None:
+    def associate(self, address_row_id: int, tx_row_id: int) -> bool:
         assert address_row_id > 0
         assert tx_row_id > 0
-        columns = (
-            self.ColumnEnum.ADDRESS_ROW_ID,
-            self.ColumnEnum.TX_ROW_ID)
-        cursor.execute(
-            f"INSERT OR IGNORE INTO {self} ("
-            f"{Column.join(columns)})"
-            f" VALUES ({ColumnValue.join(len(columns))})",
-            (address_row_id, tx_row_id))
+        return self.insert([
+            ColumnValue(self.ColumnEnum.ADDRESS_ROW_ID, address_row_id),
+            ColumnValue(self.ColumnEnum.TX_ROW_ID, tx_row_id)
+        ]) > 0
 
-    def statementSelectTransactions(
+    def filter(
             self,
-            address_row_id: int) -> Tuple[str, List[Any]]:
+            address_row_id: int) -> tuple[str, list[ColumnValueType]]:
+        from .address import AddressListTable
         assert address_row_id > 0
         return (
             (
+                f"{AddressListTable.ColumnEnum.ROW_ID} IN ("
                 f"SELECT {self.ColumnEnum.TX_ROW_ID}"
                 f" FROM {self}"
-                f" WHERE {self.ColumnEnum.ADDRESS_ROW_ID} == ?"
+                f" WHERE {self.ColumnEnum.ADDRESS_ROW_ID} == ?)"
             ),
             [address_row_id]
         )
