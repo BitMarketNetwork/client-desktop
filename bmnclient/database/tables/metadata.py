ee3f04a973edc0d6e1786b5589c5a2c1c83a37b9
dev <dev@bitmarket.network>
2022-04-06 00:40:00 +0300

    database.tables.metadata.MetadataTable optimization

--- bmnclient/database/tables/metadata.py
+++ bmnclient/database/tables/metadata.py
@@ -1,28 +1,24 @@
 from __future__ import annotations
 
 from enum import Enum
-from typing import TYPE_CHECKING
 
-from .table import AbstractTable, Column, ColumnEnum, ColumnValue, RowListProxy
-
-if TYPE_CHECKING:
-    from typing import Final, Optional, Type, Union
+from .table import AbstractTable, Column, ColumnEnum, ColumnValue
 
 
 class MetadataTable(AbstractTable, name="metadata"):
     class ColumnEnum(ColumnEnum):
-        ROW_ID: Final = ()
-        KEY: Final = ("key", "TEXT NOT NULL UNIQUE")
-        VALUE: Final = ("value", "TEXT")
+        ROW_ID = ()
+        KEY = ("key", "TEXT NOT NULL UNIQUE")
+        VALUE = ("value", "TEXT")
 
     class Key(Enum):
-        VERSION: Final = "version"
+        VERSION = "version"
 
     def get(
             self,
             key: Key,
-            value_type: Type[Union[int, str]],
-            default_value: Optional[int, str] = None) -> Optional[int, str]:
+            value_type: type(int) | type(str),
+            default_value: int | str | None = None) -> int | str | None:
         with self._database.transaction(allow_in_transaction=True) as c:
             try:
                 c.execute(
@@ -45,7 +41,7 @@ class MetadataTable(AbstractTable, name="metadata"):
         except (TypeError, ValueError):
             return default_value
 
-    def set(self, key: Key, value: Optional[int, str]) -> None:
+    def set(self, key: Key, value: int | str | None) -> None:
         with self._database.transaction(allow_in_transaction=True) as c:
             c.execute(
                 f"UPDATE {self}"
