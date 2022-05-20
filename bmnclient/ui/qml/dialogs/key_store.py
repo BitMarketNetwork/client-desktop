340a133741ea7bd5424debd957f2136f3073b2fa
Rafael Fazliyev <astico198@gmail.com>
2022-05-20 11:04:45 +0300

    ui.dialogs.key_store -> remove duplicate

--- bmnclient/ui/qml/dialogs/key_store.py
+++ bmnclient/ui/qml/dialogs/key_store.py
@@ -441,55 +441,6 @@ class AbstractTxApproveDialog(AbstractDialog):
 
 
 class TxApproveDialog(AbstractTxApproveDialog):
-    class ErrorDialog(AbstractMessageDialog):
-        def __init__(
-                self,
-                manager: DialogManager,
-                parent: TxApproveDialog,
-                *,
-                text: str):
-            super().__init__(
-                manager,
-                parent,
-                title=parent.title,  # noqa
-                text=text)
-
-        def onClosed(self) -> None:
-            PasswordDialog(self._manager, self._parent).open()
-
-    class PasswordDialog(AbstractPasswordDialog):
-        def __init__(
-                self,
-                manager: DialogManager,
-                parent: TxApproveDialog) -> None:
-            super().__init__(
-                manager,
-                parent,
-                title=parent.title) # noqa
-
-        def onPasswordAccepted(self, password: str) -> None:
-            result = self._manager.context.keyStore.native.revealSeedPhrase(
-                password)
-            if isinstance(result, str):
-                self._parent.text = result
-                return
-            if result == KeyStoreError.ERROR_INVALID_PASSWORD:
-                # noinspection PyTypeChecker
-                text = self.tr("Wrong Key Store password.")
-            elif result == KeyStoreError.ERROR_SEED_NOT_FOUND:
-                # noinspection PyTypeChecker
-                text = self.tr("Seed Phrase not found.")
-            else:
-                # noinspection PyTypeChecker
-                text = self.tr("Unknown Key Store error.")
-            self._parent.ErrorDialog(
-                self._manager,
-                self._parent,
-                text=text).open()
-
-        def onRejected(self) -> None:
-            self._parent.close.emit()
-
     def __init__(self, manager: DialogManager, coin: Coin) -> None:
         super().__init__(manager)
         self._qml_properties["type"] = self.Type.Prepare.value
