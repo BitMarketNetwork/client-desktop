a1f1eb4d726c1777bc00828977c5f6b0dacaa06b
Rafael Fazliyev <astico198@gmail.com>
2022-05-20 09:39:33 +0300

    ui.qml.dialogs.key_store -> Remove copypaste

--- bmnclient/ui/qml/dialogs/key_store.py
+++ bmnclient/ui/qml/dialogs/key_store.py
@@ -26,6 +26,57 @@ def createKeyStorePasswordDialog(manager: DialogManager) -> AbstractDialog:
         return KeyStoreNewPasswordDialog(manager)
 
 
+class PasswordDialog(AbstractPasswordDialog):
+    def __init__(
+            self,
+            manager: DialogManager,
+            parent: AbstractDialog) -> None:
+        super().__init__(
+            manager,
+            parent,
+            title=parent.title) # noqa
+
+    def onPasswordAccepted(self, password: str) -> None:
+        result = self._manager.context.keyStore.native.revealSeedPhrase(
+            password)
+        if isinstance(result, str):
+            self._parent.text = result
+            return
+        if result == KeyStoreError.ERROR_INVALID_PASSWORD:
+            # noinspection PyTypeChecker
+            text = self.tr("Wrong Key Store password.")
+        elif result == KeyStoreError.ERROR_SEED_NOT_FOUND:
+            # noinspection PyTypeChecker
+            text = self.tr("Seed Phrase not found.")
+        else:
+            # noinspection PyTypeChecker
+            text = self.tr("Unknown Key Store error.")
+        ErrorDialog(
+            self._manager,
+            self._parent,
+            text=text).open()
+
+    def onRejected(self) -> None:
+        self._parent.close.emit()
+
+
+class ErrorDialog(AbstractMessageDialog):
+    def __init__(
+            self,
+            manager: DialogManager,
+            parent: AbstractDialog,
+            *,
+            text: str):
+        super().__init__(
+            manager,
+            parent,
+            title=parent.title,  # noqa
+            text=text)
+
+    def onClosed(self) -> None:
+        PasswordDialog(self._manager, self._parent).open()
+
+
 class KeyStoreErrorDialog(AbstractMessageDialog):
     _ERROR_MAP: Final = {
         KeyStoreError.ERROR_UNKNOWN:
@@ -325,55 +376,6 @@ class RestoreSeedPhraseDialog(AbstractSeedPhraseDialog):
 
 
 class RevealSeedPhraseDialog(AbstractSeedPhraseDialog):
-    class ErrorDialog(AbstractMessageDialog):
-        def __init__(
-                self,
-                manager: DialogManager,
-                parent: RevealSeedPhraseDialog,
-                *,
-                text: str):
-            super().__init__(
-                manager,
-                parent,
-                title=parent.title,  # noqa
-                text=text)
-
-        def onClosed(self) -> None:
-            self._parent.PasswordDialog(self._manager, self._parent).open()
-
-    class PasswordDialog(AbstractPasswordDialog):
-        def __init__(
-                self,
-                manager: DialogManager,
-                parent: RevealSeedPhraseDialog) -> None:
-            super().__init__(
-                manager,
-                parent,
-                title=parent.title) # noqa
-
-        def onPasswordAccepted(self, password: str) -> None:
-            result = self._manager.context.keyStore.native.revealSeedPhrase(
-                password)
-            if isinstance(result, str):
-                self._parent.text = result
-                return
-            if result == KeyStoreError.ERROR_INVALID_PASSWORD:
-                # noinspection PyTypeChecker
-                text = self.tr("Wrong Key Store password.")
-            elif result == KeyStoreError.ERROR_SEED_NOT_FOUND:
-                # noinspection PyTypeChecker
-                text = self.tr("Seed Phrase not found.")
-            else:
-                # noinspection PyTypeChecker
-                text = self.tr("Unknown Key Store error.")
-            self._parent.ErrorDialog(
-                self._manager,
-                self._parent,
-                text=text).open()
-
-        def onRejected(self) -> None:
-            self._parent.close.emit()
-
     def __init__(self, manager: DialogManager) -> None:
         super().__init__(manager)
         self._qml_properties["type"] = self.Type.Reveal.value
@@ -383,7 +385,7 @@ class RevealSeedPhraseDialog(AbstractSeedPhraseDialog):
             self._qml_properties["readOnly"] = True
 
     def onOpened(self) -> None:
-        self.PasswordDialog(self._manager, self).open()
+        PasswordDialog(self._manager, self).open()
 
 
 class SeedSaltDialog(AbstractDialog):
@@ -412,6 +414,7 @@ class SeedSaltDialog(AbstractDialog):
     def onRejected(self) -> None:
         self._parent.reject.emit()
 
+
 class AbstractTxApproveDialog(AbstractDialog):
     _QML_NAME = "BTxApproveDialog"
     _textChanged = QSignal()
@@ -436,6 +439,7 @@ class AbstractTxApproveDialog(AbstractDialog):
             # noinspection PyUnresolvedReferences
             self._textChanged.emit()
 
+
 class TxApproveDialog(AbstractTxApproveDialog):
     class ErrorDialog(AbstractMessageDialog):
         def __init__(
@@ -451,7 +455,7 @@ class TxApproveDialog(AbstractTxApproveDialog):
                 text=text)
 
         def onClosed(self) -> None:
-            self._parent.PasswordDialog(self._manager, self._parent).open()
+            PasswordDialog(self._manager, self._parent).open()
 
     class PasswordDialog(AbstractPasswordDialog):
         def __init__(
@@ -497,4 +501,4 @@ class TxApproveDialog(AbstractTxApproveDialog):
             self._qml_properties["readOnly"] = True
 
     def onOpened(self) -> None:
-        self.PasswordDialog(self._manager, self).open()
+        PasswordDialog(self._manager, self).open()
