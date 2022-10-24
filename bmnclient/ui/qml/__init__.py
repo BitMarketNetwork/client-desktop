568df513cd0adf637d98a12430f25ace3a52d380
Integer128 <59063176+integer128@users.noreply.github.com>
2022-10-24 12:15:40 +0300

    ui.qml.__init__ -> removed unused import

--- bmnclient/ui/qml/__init__.py
+++ bmnclient/ui/qml/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import os
-from typing import TYPE_CHECKING, Sequence
+from typing import TYPE_CHECKING
 
 from PySide6.QtCore import \
     Property as QProperty, \
